429d0139cf5750fe9abefc4a06a6ac50d0639f2d
Justin Eldridge <eldridgejm@gmail.com>
2021-09-16 11:56:33 -0700

    Added workflows.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dictconfig",
-    version="0.0.1",
+    version="0.0.0",
     packages=find_packages(),
     install_requires=[],
     tests_require=["pytest"],
