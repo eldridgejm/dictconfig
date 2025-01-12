83c9c8f19b4612b0de73962531e59a482f5f8031
Justin Eldridge <eldridgejm@gmail.com>
2025-01-12 11:53:42 -0800

    Updated flake and github action.

--- flake.nix
+++ flake.nix
@@ -1,29 +1,29 @@
 {
   description = "A recursive, resolvable dictionary.";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/21.11;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-24.11;
 
-  outputs = { self, nixpkgs }: 
-    let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-    in
-      {
-        dictconfig = forAllSystems (system:
-          with import nixpkgs { system = "${system}"; };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+  in {
+    dictconfig = forAllSystems (
+      system:
+        with import nixpkgs {system = "${system}";};
+          python3Packages.buildPythonPackage {
+            name = "dictconfig";
+            src = ./.;
+            propagatedBuildInputs = with python3Packages; [jinja2];
+            nativeBuildInputs = with python3Packages; [pytest black ipython sphinx sphinx_rtd_theme];
+          }
+    );
 
-            python3Packages.buildPythonPackage {
-              name = "dictconfig";
-              src = ./.;
-              propagatedBuildInputs = with python3Packages; [ jinja2 ];
-              nativeBuildInputs = with python3Packages; [ pytest black ipython sphinx sphinx_rtd_theme ];
-            }
-
-          );
-
-        defaultPackage = forAllSystems (system:
-            self.dictconfig.${system}
-          );
-      };
-
+    defaultPackage = forAllSystems (
+      system:
+        self.dictconfig.${system}
+    );
+  };
 }
